3c1bf8a08f8b5970b51450aa821cddd57074f249
Tauheed Elahee <36247967+Tauheed-Elahee@users.noreply.github.com>
2019-02-17 08:22:39 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
 
+from gpiozero import Servo
+from time import sleep
+
 
 class Brew(MycroftSkill):
     def __init__(self):
@@ -8,6 +11,13 @@ class Brew(MycroftSkill):
     @intent_file_handler('brew.intent')
     def handle_brew(self, message):
         self.speak_dialog('brew')
+        servo = Servo(17)
+        servo.min()
+        sleep(1)
+        servo.max()
+        sleep(1)
+        servo.min()
+        sleep(1)
 
 
 def create_skill():
